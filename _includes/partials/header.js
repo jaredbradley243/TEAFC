afcadd415bdb3104c4f0009858d0687cd156ec94
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-07-14 16:13:07 -0700

    Speed up menu transition

--- _includes/partials/header.js
+++ _includes/partials/header.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function () {
     menu.classList.add(
       "transition",
       "ease-out",
-      "duration-200",
+      "duration-20",
       "opacity-0",
       "translate-y-1"
     );
@@ -26,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function () {
         menu.classList.remove("opacity-100", "translate-y-0");
         setTimeout(() => {
           menu.style.display = "none";
-        }, 200); // Match the duration of the transition
+        }, 20); // Match the duration of the transition
       } else {
         menu.style.display = "block";
         setTimeout(() => {
