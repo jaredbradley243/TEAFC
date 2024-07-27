8e0b053ad8b40b3febd902d239a8ab91111756ea
Shark-with-a-Drill <108551190+Shark-with-a-Drill@users.noreply.github.com>
2024-07-26 23:09:53 -0700

    Add image slider with unlimited amounts of images
    Supports touch input, accessibility via tab, arrow keys, and screen reader, and standard mouse input

--- _includes/partials/accordion.js
+++ _includes/partials/accordion.js
@@ -4,20 +4,11 @@ accordionHeader.forEach(accordionHeader => {
     accordionHeader.addEventListener("click", event => {
         accordionHeader.classList.toggle("active")
         const accordionBody = accordionHeader.nextElementSibling
-        const expanded = accordionHeader.getAttribute('aria-expanded') === 'true';
-        if(!expanded) {
+        if(accordionHeader.classList.contains('active')) {
             accordionBody.style.maxHeight = accordionBody.scrollHeight + "px"
         }
         else {
             accordionBody.style.maxHeight = '0px'
         }
     })
-    const accordionBody = accordionHeader.nextElementSibling;
-        accordionBody.addEventListener('transitionend', () => {
-            if (accordionHeader.getAttribute('aria-expanded') === 'false') {
-                accordionBody.style.maxHeight = '';
-            } else {
-                accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
-            }
-        })
 })
