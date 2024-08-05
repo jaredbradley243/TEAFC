bfb9eb577383ae79b0f61f6a372cb199b7bf0938
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-08-05 14:03:17 -0700

    Enable accordions to be tab-browsed
    without selecting inside content when accordion is closed.
    -Amjad

--- _includes/partials/accordion.js
+++ _includes/partials/accordion.js
@@ -1,21 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const accordionHeaders = document.querySelectorAll('.accordion-header');
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionHeaders = document.querySelectorAll(".accordion-header");
 
-    accordionHeaders.forEach(accordionHeader => {
-        const accordionBody = accordionHeader.nextElementSibling;
+  accordionHeaders.forEach((accordionHeader) => {
+    const accordionBody = accordionHeader.nextElementSibling;
 
-        accordionHeader.addEventListener('click', event => {
-            const isExpanded = accordionHeader.getAttribute('aria-expanded') === 'true';
+    accordionHeader.addEventListener("click", (event) => {
+      const isExpanded =
+        accordionHeader.getAttribute("aria-expanded") === "true";
 
-            if (!isExpanded) {
-                accordionBody.style.maxHeight = accordionBody.scrollHeight + 'px';
-                accordionHeader.setAttribute('aria-expanded', 'true');
-            } else {
-                accordionBody.style.maxHeight = '0px';
-                accordionHeader.setAttribute('aria-expanded', 'false');
-            }
+      if (!isExpanded) {
+        accordionBody.classList.remove("hidden");
+        accordionBody.style.maxHeight = accordionBody.scrollHeight + "px";
+        accordionHeader.setAttribute("aria-expanded", "true");
+      } else {
+        accordionBody.style.maxHeight = "0px";
+        accordionHeader.setAttribute("aria-expanded", "false");
+        setTimeout(() => {
+          accordionBody.classList.add("hidden");
+        }, 256);
+      }
 
-            accordionHeader.classList.toggle('active');
-        });
+      accordionHeader.classList.toggle("active");
     });
+  });
 });
