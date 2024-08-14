92027f11a3338a3c3dcf624cf1184f169a92a694
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-08-13 18:57:43 -0700

    Add "Read More" button to founder's letter (WIP; needs animation)
    The founders letter was too long for mobile site. I added a "Read More" button that would expand the text should a visitor be interested in reading the full letter.

--- _includes/partials/founders.js
+++ _includes/partials/founders.js
@@ -1,10 +1,10 @@
 // <!-- /_includes/partials/founders.js -->
-
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   const hiddenText = document.getElementById("founders-hidden-text");
   const readMoreButton = document.getElementById("founders-read-more-button");
-
+  const readMoreButtonArrow = readMoreButton.querySelector("svg");
   readMoreButton.addEventListener("click", () => {
     hiddenText.classList.toggle("hidden");
+    readMoreButtonArrow.classList.toggle("rotate-180");
   });
 });
