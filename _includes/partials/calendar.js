0f2bb428ebddaf885615bee287de1597d6818b8f
Amjad Afzal <108551190+Shark-with-a-Drill@users.noreply.github.com>
2024-08-29 16:40:51 -0700

    Calendar.JS - add event markers

--- _includes/partials/calendar.js
+++ _includes/partials/calendar.js
@@ -10,7 +10,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const sixMonthsAgo = new Date(formattedYear, formattedMonth - 6);
   const modifiedURL = `${endpoint}&timeMin=${encodeURIComponent(sixMonthsAgo.toISOString())}&singleEvents=true`;
 
-  // console.log(modifiedURL)
+  const blueStar = '/assets/img/star-blue.svg';
+  const orangeStar = '/assets/img/star-orange.svg';
 
   const prevMonthBtn = document.querySelector("#prevMonth");
   const nextMonthBtn = document.querySelector("#nextMonth");
@@ -34,9 +35,82 @@ document.addEventListener("DOMContentLoaded", function () {
         year: "numeric",
       });
     const totalCells = 42;
+
     const renderMonthName = () => {
       monthTitle.innerText = monthName();
     };
+
+    // const renderCurrentMonth = () => {
+    //   const fragment = document.createDocumentFragment();
+    //   const currentMonthGridStart = (firstDayNumber() + 6) % 7;
+    //   const lastMonthEndDay = currentMonthGridStart - 1;
+    //   const numDaysCurrentMonth = daysInMonth();
+    //   const numDaysPrevMonth = daysInPrevMonth();
+    //   const prevMonthGridStart = numDaysPrevMonth - lastMonthEndDay;
+    //   const numDaysNextMonth =
+    //     totalCells - (currentMonthGridStart + numDaysCurrentMonth);
+
+    //   renderMonthName();
+
+    //   //fill grid with prev month's days
+    //   for (let day = prevMonthGridStart; day <= numDaysPrevMonth; day++) {
+    //     const cell = document.createElement("div");
+    //     const cellDate = document.createElement("time");
+    //     let formattedMonth = currentMonth;
+    //     formattedMonth =
+    //       formattedMonth < 10 ? "0" + formattedMonth : formattedMonth;
+    //     const formattedDay = day < 10 ? "0" + day : day;
+    //     const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
+    //     cell.className =
+    //       "relative flex flex-col gap-1 items-center justify-center bg-gray-50 py-1.5 text-gray-400";
+    //     fragment.appendChild(cell);
+    //     cellDate.textContent = day;
+    //     cellDate.setAttribute("datetime", dateString);
+    //     cellDate.className =
+    //       "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
+    //     cell.appendChild(cellDate);
+    //   }
+
+    //   //fill grid with current month's days
+    //   for (let day = 1; day <= numDaysCurrentMonth; day++) {
+    //     const cell = document.createElement("div");
+    //     const cellDate = document.createElement("time");
+    //     let formattedMonth = currentMonth + 1;
+    //     formattedMonth =
+    //       formattedMonth < 10 ? "0" + formattedMonth : formattedMonth;
+    //     const formattedDay = day < 10 ? "0" + day : day;
+    //     const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
+    //     cell.className =
+    //       "relative flex flex-col gap-1 justify-center bg-white py-1.5 flex items-center text-gray-900";
+    //     fragment.appendChild(cell);
+    //     cellDate.textContent = day;
+    //     cellDate.setAttribute("datetime", dateString);
+    //     cellDate.className =
+    //       "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
+    //     cell.appendChild(cellDate);
+    //   }
+
+    //   //fill grid with next month's days
+    //   for (let day = 1; day <= numDaysNextMonth; day++) {
+    //     const cell = document.createElement("div");
+    //     const cellDate = document.createElement("time");
+    //     let formattedMonth = currentMonth + 2;
+    //     formattedMonth =
+    //       formattedMonth < 10 ? "0" + formattedMonth : formattedMonth;
+    //     const formattedDay = day < 10 ? "0" + day : day;
+    //     const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
+    //     cell.className =
+    //       "relative flex flex-col gap-1 bg-gray-50 py-1.5 flex justify-center items-center text-gray-400";
+    //     fragment.appendChild(cell);
+    //     cellDate.textContent = day;
+    //     cellDate.setAttribute("datetime", dateString);
+    //     cellDate.className =
+    //       "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
+    //     cell.appendChild(cellDate);
+    //   }
+
+    //   return fragment;
+    // };
 
     const renderCurrentMonth = () => {
       const fragment = document.createDocumentFragment();
@@ -45,70 +119,55 @@ document.addEventListener("DOMContentLoaded", function () {
       const numDaysCurrentMonth = daysInMonth();
       const numDaysPrevMonth = daysInPrevMonth();
       const prevMonthGridStart = numDaysPrevMonth - lastMonthEndDay;
-      const numDaysNextMonth =
-        totalCells - (currentMonthGridStart + numDaysCurrentMonth);
-
+      const numDaysNextMonth = totalCells - (currentMonthGridStart + numDaysCurrentMonth);
+    
       renderMonthName();
-
-      //fill grid with prev month's days
+    
+      // Helper function to create a cell
+      const createCell = (day, formattedMonth, isCurrentMonth, isNextMonth) => {
+        const cell = document.createElement("div");
+        const cellDate = document.createElement("time");
+    
+        const formattedDay = day < 10 ? "0" + day : day;
+        const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
+    
+        cell.className = `relative flex flex-col gap-1 items-center justify-center py-1.5 ${
+          isCurrentMonth ? "bg-white text-gray-900" : "bg-gray-50 text-gray-400"
+        }`;
+    
+        if (isNextMonth) {
+          cell.classList.add("bg-gray-50", "text-gray-400");
+        }
+    
+        cellDate.textContent = day;
+        cellDate.setAttribute("datetime", dateString);
+        cellDate.className = "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
+    
+        cell.appendChild(cellDate);
+        return cell;
+      };
+    
+      // Fill grid with prev month's days
       for (let day = prevMonthGridStart; day <= numDaysPrevMonth; day++) {
-        const cell = document.createElement("div");
-        const cellDate = document.createElement("time");
-        let formattedMonth = currentMonth;
-        formattedMonth =
-          formattedMonth < 10 ? "0" + formattedMonth : formattedMonth;
-        const formattedDay = day < 10 ? "0" + day : day;
-        const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
-        cell.className =
-          "relative flex items-center bg-gray-50 py-1.5 text-gray-400"; // Tailwind class for empty cells
-        fragment.appendChild(cell);
-        cellDate.textContent = day;
-        cellDate.setAttribute("datetime", dateString);
-        cellDate.className =
-          "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
-        cell.appendChild(cellDate);
+        const formattedMonth = currentMonth < 10 ? "0" + currentMonth : currentMonth;
+        fragment.appendChild(createCell(day, formattedMonth, false));
       }
-
-      //fill grid with current month's days
+    
+      // Fill grid with current month's days
       for (let day = 1; day <= numDaysCurrentMonth; day++) {
-        const cell = document.createElement("div");
-        const cellDate = document.createElement("time");
-        let formattedMonth = currentMonth + 1;
-        formattedMonth =
-          formattedMonth < 10 ? "0" + formattedMonth : formattedMonth;
-        const formattedDay = day < 10 ? "0" + day : day;
-        const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
-        cell.className =
-          "relative bg-white py-1.5 flex items-center text-gray-900";
-        fragment.appendChild(cell);
-        cellDate.textContent = day;
-        cellDate.setAttribute("datetime", dateString);
-        cellDate.className =
-          "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
-        cell.appendChild(cellDate);
+        const formattedMonth = currentMonth + 1 < 10 ? "0" + (currentMonth + 1) : currentMonth + 1;
+        fragment.appendChild(createCell(day, formattedMonth, true));
       }
-
-      //fill grid with next month's days
-      for (let day = 0; day < numDaysNextMonth; day++) {
-        const cell = document.createElement("div");
-        const cellDate = document.createElement("time");
-        let formattedMonth = currentMonth + 2;
-        formattedMonth =
-          formattedMonth < 10 ? "0" + formattedMonth : formattedMonth;
-        const formattedDay = day < 10 ? "0" + day : day;
-        const dateString = `${currentYear}-${formattedMonth}-${formattedDay}`;
-        cell.className =
-          "relative bg-gray-50 py-1.5 flex items-center text-gray-400";
-        fragment.appendChild(cell);
-        cellDate.textContent = day + 1;
-        cellDate.setAttribute("datetime", dateString);
-        cellDate.className =
-          "mx-auto flex h-7 w-7 items-center justify-center rounded-full";
-        cell.appendChild(cellDate);
+    
+      // Fill grid with next month's days
+      for (let day = 1; day <= numDaysNextMonth; day++) {
+        const formattedMonth = currentMonth + 2 < 10 ? "0" + (currentMonth + 2) : currentMonth + 2;
+        fragment.appendChild(createCell(day, formattedMonth, false, true));
       }
-
+    
       return fragment;
     };
+    
 
     const clearCalendar = () => {
       while (calendarGrid.firstChild) {
@@ -127,7 +186,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const updateCalendar = () => {
       clearCalendar();
       calendarGrid.appendChild(renderCurrentMonth());
-      highlightToday();
+      sortCalData();
     };
 
     const renderPrevMonth = () => {
@@ -155,36 +214,40 @@ document.addEventListener("DOMContentLoaded", function () {
           throw new Error("Failed to fetch events");
         }
         const calData = await response.json();
-        // console.log(eventData);
         eventData = calData.items || [];
-        // console.log(eventData[0].summary, 'start:', eventData[0].start.dateTime, 'end:', eventData[0].end.dateTime);
-        console.log(eventData)
-        // sortCalData();
+        sortCalData();
         return eventData;
       } catch (error) {
         console.error('Error fetching events:', error);
       }
     }
 
-    // const sortCalData = () => {
-    //   eventData.forEach(event => {
-    //     let timedEvent;
-    //     let allDayEvent;
-    //     let eventCell;
-    //     if (timedEvent = event.start.dateTime) {
-    //       // const eventDate = timedEvent.slice(11, 19)
-    //       // console.log(timedEvent, 'timed event')
-    //       // console.log(eventDate)
-    //     } else if (allDayEvent = event.start.date) {
-    //       eventCell = document.querySelector(`[datetime = "${allDayEvent}"]`)
-    //       console.log(allDayEvent, 'all day')
-    //       console.log(eventCell);
-    //     }
-    //     // let todaysDate = event.start.dateTime || event.start.date;
-    //     // todaysDate = todaysDate
-    //     // console.log(todaysDate);
-    //   })
-    // }
+    const sortCalData = () => {
+      // console.log(new Date(), 'inside');
+      // console.log(eventData);
+      eventData.forEach(event => {
+        // console.log(event, event.summary)
+        const timedEventStart = event.start.dateTime;
+        const allDayEvent = event.start.date;
+        const eventName = event.summary;
+        const imgElement = document.createElement('img');
+        imgElement.src = blueStar;
+        imgElement.className = "h-5 w-5";
+        let eventCell = { name: eventName };
+    
+        if (timedEventStart || allDayEvent) {
+          const eventDate = timedEventStart ? new Date(timedEventStart).toISOString().split('T')[0] : allDayEvent;
+          eventCell.dateElement = document.querySelector(`[datetime="${eventDate}"]`);
+          // console.log(eventCell.dateElement);
+    
+        if (eventCell.dateElement && !eventCell.dateElement.nextElementSibling) {
+          eventCell.dateElement.insertAdjacentElement('afterend', imgElement);
+        }
+        }
+    
+        highlightToday();
+      });
+    }
 
     return {
       highlightToday,
@@ -203,7 +266,6 @@ document.addEventListener("DOMContentLoaded", function () {
   );
 
   calendarGrid.appendChild(calendar.renderCurrentMonth());
-  calendar.highlightToday();
   nextMonthBtn.addEventListener("click", calendar.renderNextMonth);
   prevMonthBtn.addEventListener("click", calendar.renderPrevMonth);
   calendar.fetchCalData(modifiedURL);
