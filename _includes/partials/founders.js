833c6279b72c359205e1102b5d701cf4e34dcc1b
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-09-03 19:39:32 -0700

    founders.js: add condition to check if hiddenText exists before running script

--- _includes/partials/founders.js
+++ _includes/partials/founders.js
@@ -1,21 +1,22 @@
 // <!-- /_includes/partials/founders.js -->
 document.addEventListener("DOMContentLoaded", function () {
   const hiddenText = document.getElementById("founders-hidden-text");
-  const readMoreButton = document.getElementById("founders-read-more-button");
-  const readMoreButtonArrow = readMoreButton.querySelector("svg");
-  readMoreButton.addEventListener("click", () => {
-    
-    readMoreButtonArrow.classList.toggle("rotate-180");
-    if (hiddenText.classList.contains('hidden')) {
-      hiddenText.classList.toggle("hidden");
-      hiddenText.style.maxHeight = hiddenText.scrollHeight + "px";
-      readMoreButton.setAttribute("aria-expanded", "true");
-    } else {
-      hiddenText.style.maxHeight = "0px";
-      readMoreButton.setAttribute("aria-expanded", "false");
-      setTimeout(() => {
-        hiddenText.classList.add("hidden");
-      }, 256);
-    }
-  });
+  if (hiddenText) {
+    const readMoreButton = document.getElementById("founders-read-more-button");
+    const readMoreButtonArrow = readMoreButton.querySelector("svg");
+    readMoreButton.addEventListener("click", () => {
+      readMoreButtonArrow.classList.toggle("rotate-180");
+      if (hiddenText.classList.contains("hidden")) {
+        hiddenText.classList.toggle("hidden");
+        hiddenText.style.maxHeight = hiddenText.scrollHeight + "px";
+        readMoreButton.setAttribute("aria-expanded", "true");
+      } else {
+        hiddenText.style.maxHeight = "0px";
+        readMoreButton.setAttribute("aria-expanded", "false");
+        setTimeout(() => {
+          hiddenText.classList.add("hidden");
+        }, 256);
+      }
+    });
+  }
 });
