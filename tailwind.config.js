b777d88cb810a6d8c726a8cc80d7fcf143afad76
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-09-05 19:56:57 -0700

    404 Page: Add animation to page

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,84 @@ module.exports = {
         "40%": "40%",
         "50%": "50%",
       },
+      keyframes: {
+        floatUp: {
+          "0%": {
+            transform: "translateY(0) rotate(0deg)",
+          },
+          "10%": {
+            transform: "translateY(-10%) rotate(3deg)",
+          },
+          "20%": {
+            transform: "translateY(-20%) rotate(-3deg)",
+          },
+          "30%": {
+            transform: "translateY(-30%) rotate(2deg)",
+          },
+          "40%": {
+            transform: "translateY(-40%) rotate(-2deg)",
+          },
+          "50%": {
+            transform: "translateY(-50%) rotate(1deg)",
+          },
+          "60%": {
+            transform: "translateY(-60%) rotate(-1deg)",
+          },
+          "70%": {
+            transform: "translateY(-70%) rotate(0deg)",
+          },
+          "80%": {
+            transform: "translateY(-80%) rotate(-1deg)",
+          },
+          "90%": {
+            transform: "translateY(-90%) rotate(1deg)",
+          },
+          "100%": {
+            transform: "translateY(-150%) rotate(0deg)",
+            visibility: "hidden",
+          },
+        },
+        shrinkContainer: {
+          "0%": {
+            marginTop: "0%",
+          },
+          "10%": {
+            marginTop: "-10%",
+          },
+          "20%": {
+            marginTop: "-20%",
+          },
+          "30%": {
+            marginTop: "-30%",
+          },
+          "40%": {
+            marginTop: "-40%",
+          },
+          "50%": {
+            marginTop: "-50%",
+          },
+          "60%": {
+            marginTop: "-60%",
+          },
+          "70%": {
+            marginTop: "-70%",
+          },
+          "80%": {
+            marginTop: "-70%",
+          },
+          "90%": {
+            marginTop: "-70%",
+          },
+          "100%": {
+            marginTop: "-70%",
+            visibility: "hidden",
+          },
+        },
+      },
+      animation: {
+        "float-up": "floatUp 5s linear forwards 0.5s",
+        "shrink-container": "shrinkContainer 5s linear forwards 0.5s",
+      },
     },
   },
   plugins: [
