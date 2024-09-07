ca89f3e5ad1d7748a9be72101ec4d5ed2f2b30bd
Amjad Afzal <108551190+Shark-with-a-Drill@users.noreply.github.com>
2024-09-06 22:27:26 -0700

    Img-slider - fix query selector logic for mobile sliding

--- _includes/partials/img-slider.js
+++ _includes/partials/img-slider.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const slider = document.querySelector(".slider");
-  const slideLeftButton = document.querySelectorAll(".slide-left", "left-arrow");
-  const slideRightButton = document.querySelectorAll(".slide-right", "right-arrow");
+  const slideLeftButton = document.querySelectorAll(".slide-left-container");
+  const slideRightButton = document.querySelectorAll(".slide-right-container");
 
   slideRightButton.forEach((element) => {
     element.addEventListener("click", () => slider.scrollBy(15, 0));
