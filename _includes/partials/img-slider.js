7d237d1805f8d460eccdf3bbbc623c1b3582b289
Amjad Afzal <108551190+Shark-with-a-Drill@users.noreply.github.com>
2024-09-06 22:06:40 -0700

    Img-slider.js - increase slide distance for mobile swipe bug

--- _includes/partials/img-slider.js
+++ _includes/partials/img-slider.js
@@ -4,10 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
   const slideRightButton = document.querySelectorAll(".slide-right", "right-arrow");
 
   slideRightButton.forEach((element) => {
-    element.addEventListener("click", () => slider.scrollBy(5, 0));
+    element.addEventListener("click", () => slider.scrollBy(15, 0));
   });
 
   slideLeftButton.forEach((element) => {
-    element.addEventListener("click", () => slider.scrollBy(-5, 0));
+    element.addEventListener("click", () => slider.scrollBy(-15, 0));
   });
 });
