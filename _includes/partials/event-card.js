9810486e8ad69c490e3ed93ad63b35a7f0851c16
Jared Bradley <107898107+jaredbradley243@users.noreply.github.com>
2024-10-14 20:55:40 -0700

    Eventcard.js: Fix bug where event read more button doesn't work when any event card doesn't have hidden text

--- _includes/partials/event-card.js
+++ _includes/partials/event-card.js
@@ -3,56 +3,58 @@ document.addEventListener("DOMContentLoaded", function () {
   const eventCards = document.querySelectorAll(".event-card-main");
   eventCards.forEach((card) => {
     const hiddenText = card.querySelector(".event-card-hidden-text");
-    const imgContainer = card.querySelector(".event-card-img-holder");
-    const readMoreButton = hiddenText.nextElementSibling;
-    const readMoreButtonArrow = readMoreButton.querySelector("svg");
+    if (hiddenText) {
+      const imgContainer = card.querySelector(".event-card-img-holder");
+      const readMoreButton = hiddenText.nextElementSibling;
+      const readMoreButtonArrow = readMoreButton.querySelector("svg");
 
-    let isDesktopView = window.innerWidth >= 1024;
+      let isDesktopView = window.innerWidth >= 1024;
 
-    const easeTransition = (newHeight) => {
-      imgContainer.style.height = imgContainer.scrollHeight + "px";
-      imgContainer.offsetHeight;
-      imgContainer.style.height = newHeight + "px";
-    };
+      const easeTransition = (newHeight) => {
+        imgContainer.style.height = imgContainer.scrollHeight + "px";
+        imgContainer.offsetHeight;
+        imgContainer.style.height = newHeight + "px";
+      };
 
-    const expandContent = () => {
-      const imgHeight = imgContainer.scrollHeight;
-      readMoreButtonArrow.classList.add("rotate-180");
-      hiddenText.classList.toggle("hidden");
-      hiddenText.style.maxHeight = hiddenText.scrollHeight + "px";
-      if (isDesktopView) {
-        const newHeight = imgHeight + hiddenText.scrollHeight;
-        easeTransition(newHeight);
-      }
-      readMoreButton.setAttribute("aria-expanded", "true");
-    };
+      const expandContent = () => {
+        const imgHeight = imgContainer.scrollHeight;
+        readMoreButtonArrow.classList.add("rotate-180");
+        hiddenText.classList.toggle("hidden");
+        hiddenText.style.maxHeight = hiddenText.scrollHeight + "px";
+        if (isDesktopView) {
+          const newHeight = imgHeight + hiddenText.scrollHeight;
+          easeTransition(newHeight);
+        }
+        readMoreButton.setAttribute("aria-expanded", "true");
+      };
 
-    const collapseContent = () => {
-      if (isDesktopView) {
-        const newHeight = imgContainer.scrollHeight - hiddenText.scrollHeight;
-        easeTransition(newHeight);
-      }
-      hiddenText.style.maxHeight = "0px";
-      readMoreButton.setAttribute("aria-expanded", "false");
-      setTimeout(() => {
-        hiddenText.classList.add("hidden");
-      }, 256);
-      readMoreButtonArrow.classList.remove("rotate-180");
-    };
+      const collapseContent = () => {
+        if (isDesktopView) {
+          const newHeight = imgContainer.scrollHeight - hiddenText.scrollHeight;
+          easeTransition(newHeight);
+        }
+        hiddenText.style.maxHeight = "0px";
+        readMoreButton.setAttribute("aria-expanded", "false");
+        setTimeout(() => {
+          hiddenText.classList.add("hidden");
+        }, 256);
+        readMoreButtonArrow.classList.remove("rotate-180");
+      };
 
-    readMoreButton.addEventListener("click", () => {
-      readMoreButtonArrow.classList.toggle("rotate-180");
-      if (hiddenText.classList.contains("hidden")) {
-        expandContent();
-      } else {
+      readMoreButton.addEventListener("click", () => {
+        readMoreButtonArrow.classList.toggle("rotate-180");
+        if (hiddenText.classList.contains("hidden")) {
+          expandContent();
+        } else {
+          collapseContent();
+        }
+      });
+
+      window.addEventListener("resize", () => {
+        isDesktopView = window.innerWidth >= 1024;
         collapseContent();
-      }
-    });
-
-    window.addEventListener("resize", () => {
-      isDesktopView = window.innerWidth >= 1024;
-      collapseContent();
-      imgContainer.style.height = "";
-    });
+        imgContainer.style.height = "";
+      });
+    }
   });
 });
